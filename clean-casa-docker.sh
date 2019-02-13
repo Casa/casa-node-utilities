3d3336dfb0bcd40278a1ce9c43395231b751a196
Michael Borglin <michael@team.casa>
2019-02-13 12:44:25 -0800

    add clean images and containers script

---
+++ clean-casa-docker.sh
@@ -0,0 +1,14 @@
+cd /usr/local/casa/applications/
+docker-compose -f lightning-node.yml down
+docker-compose -f manager.yml down
+docker-compose -f update-manager.yml down
+docker-compose -f logspout.yml down
+# stop docker containers
+docker stop $(docker ps -aq)
+# remove all containers
+docker rm $(docker ps -aq)
+# remove all images
+docker rmi $(docker images -q) -f
+
+cd /usr/local/casa/
+source launch.sh
