aab5f032eb3d6982ef9a07d8e486adda9e82f687
Michael Borglin <michael@team.casa>
2019-03-08 16:05:43 -0800

    add space for readability

--- resync-chain.sh
+++ resync-chain.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # This script should be used to resync the chain from Casa's server. It will only work if there is no registered user.
+
 if docker logs bitcoind 2>&1 | grep -q reindex-chainstate; then
     echo Bitcoind Chainstate error found. Fixing ...
     curl --request POST "http://127.0.0.1:3000/v1/device/resync-chain"
