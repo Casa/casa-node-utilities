ae7c8430e1c31eaed3b88327f956a3e7ae696df6
Michael Borglin <michael@team.casa>
2019-03-08 16:09:46 -0800

    only display last 100 lines

--- resync-chain.sh
+++ resync-chain.sh
@@ -4,7 +4,7 @@
 if docker logs bitcoind 2>&1 | grep -q reindex-chainstate; then
     echo Bitcoind Chainstate error found. Fixing ...
     curl --request POST "http://127.0.0.1:3000/v1/device/resync-chain"
-    docker logs manager -f
+    docker logs manager --tail 100 -f
 else
     echo Bitcoind Chainstate error not found. Please contact Casa Support at help@team.casa
 fi
