461580ce09909c63d9412cdcf80f1adf319514c0
Michael Borglin <michael@team.casa>
2019-06-11 16:36:45 -0700

    add fix missing settings file

---
+++ fix-missing-settings-file.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# delete the settings file if it exists
+sudo rm /mnt/data/volumes/applications_settings/_data/settings.json
+
+# replace with a default settings file
+sudo echo '{"bitcoind":{"bitcoinNetwork":"mainnet","bitcoindListen":true,"bitcoindTor":false},"lnd":{"chain":"bitcoin","backend":"bitcoind","lndNetwork":"mainnet","autopilot":false,"externalIP":"","lndTor":false},"system":{}' | sudo tee --append /mnt/data/volumes/applications_settings/_data/settings.json
+
