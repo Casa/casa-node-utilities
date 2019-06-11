d06cadf459af2aad27c5114e37bec16b8ed8d05a
Michael Borglin <michael@team.casa>
2019-06-11 16:41:33 -0700

    add restart command

--- fix-missing-settings-file.sh
+++ fix-missing-settings-file.sh
@@ -6,3 +6,5 @@ sudo rm /mnt/data/volumes/applications_settings/_data/settings.json
 # replace with a default settings file
 sudo echo '{"bitcoind":{"bitcoinNetwork":"mainnet","bitcoindListen":true,"bitcoindTor":false},"lnd":{"chain":"bitcoin","backend":"bitcoind","lndNetwork":"mainnet","autopilot":false,"externalIP":"","lndTor":false},"system":{}' | sudo tee --append /mnt/data/volumes/applications_settings/_data/settings.json
 
+# restart the manager
+docker restart manager
