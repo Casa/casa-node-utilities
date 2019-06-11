60852fa5832a8aea41a2c9162f3a48cbb76d8079
Michael Borglin <michael@team.casa>
2019-06-11 16:46:32 -0700

    missing closing bracket

--- fix-missing-settings-file.sh
+++ fix-missing-settings-file.sh
@@ -4,7 +4,7 @@
 sudo rm /mnt/data/volumes/applications_settings/_data/settings.json
 
 # replace with a default settings file
-sudo echo '{"bitcoind":{"bitcoinNetwork":"mainnet","bitcoindListen":true,"bitcoindTor":false},"lnd":{"chain":"bitcoin","backend":"bitcoind","lndNetwork":"mainnet","autopilot":false,"externalIP":"","lndTor":false},"system":{}' | sudo tee --append /mnt/data/volumes/applications_settings/_data/settings.json
+sudo echo '{"bitcoind":{"bitcoinNetwork":"mainnet","bitcoindListen":true,"bitcoindTor":false},"lnd":{"chain":"bitcoin","backend":"bitcoind","lndNetwork":"mainnet","autopilot":false,"externalIP":"","lndTor":false},"system":{}}' | sudo tee --append /mnt/data/volumes/applications_settings/_data/settings.json
 
 # restart the manager
 docker restart manager
