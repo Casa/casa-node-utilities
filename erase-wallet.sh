c83aac49bf683d73994704bec4a345c3952d4d05
Michael Borglin <michael@team.casa>
2019-11-24 15:32:09 -0800

    Simplify rm commands

--- erase-wallet.sh
+++ erase-wallet.sh
@@ -1,5 +1,5 @@
-docker exec lnd rm /root/.lnd/data/chain/bitcoin/mainnet/*
-docker exec lnd rm /root/.lnd/data/graph/mainnet/*
+docker exec lnd rm -rf /root/.lnd/data/chain/bitcoin/mainnet/
+docker exec lnd rm -rf /root/.lnd/data/graph/mainnet/
 docker exec manager rm /accounts/user.json
 docker restart lnd
 cd /usr/local/casa/applications
